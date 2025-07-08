dcabd48969993b0a9bc2a39899428ac8a573bdc1
Jonathan <3713684+gsmlg@users.noreply.github.com>
2025-07-08 15:13:48 +0800

    feat: Add hex_api for test and demo. (#5)
    Co-authored-by: Jonathan Gao <gsmlg.com@gmail.com>

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -65,30 +65,41 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: (context) => Center(
-        child: SizedBox(
-          width: w * 0.618,
-          height: w * 0.618,
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-            child: Center(
-              child: Column(
-                children: [
-                  LaddingPageLottie(
-                    width: w * 0.382,
-                    height: w * 0.382,
-                  ),
-                  Text(
-                    context.l10n.welcomeHome,
-                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                          color:
-                              Theme.of(context).colorScheme.secondaryContainer,
-                        ),
-                  ),
-                ],
+      body: (context) => SafeArea(
+        child: Center(
+          child: SizedBox(
+            width: w * 0.618,
+            height: w * 0.618,
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              child: Center(
+                child: Column(
+                  children: [
+                    LaddingPageLottie(
+                      width: w * 0.382,
+                      height: w * 0.382,
+                    ),
+                    Text(
+                      context.l10n.welcomeHome,
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .secondaryContainer,
+                          ),
+                    ),
+                    Text(
+                      '$screenWidth x $screenHeight',
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .secondaryContainer,
+                          ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
